cbb43be40823fa79d96e72feb031497c683c23c5
mansourfatemi <mansour.afghan@gmail.com>
2026-08-24 10:50:42 +0200

    fix: trim 5 more over-translated faraz in Hashr/Fajr (t7 fraz 3,5,70,72,73) - same duplicate-block pattern as Names-of-Allah section

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='mt-mq-v20260824104320';
+const CACHE='mt-mq-v20260824105035';
 const CORE=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 const AUDIO=[
   "./qaris/hashemi/audio/01.m4a",
