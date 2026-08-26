ff25ff18764cb09611a7377f70bc1de5b345dd33
mansourfatemi <mansour.afghan@gmail.com>
2026-08-26 13:40:50 +0200

    force SW cache refresh - bump version to invalidate old cache on devices

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='mt-mq-v20260824111855';
+const CACHE='mt-mq-v20260826134050';
 const CORE=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 const AUDIO=[
   "./qaris/hashemi/audio/01.m4a",
