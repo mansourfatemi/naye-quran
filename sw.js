9cc179c863cbc6ff68d568c088b7da37b8832f55
mansourfatemi <mansour.afghan@gmail.com>
2026-08-31 12:13:22 +0200

    Bump SW cache version to force icon refresh on all devices

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='mt-mq-v20260831120559';
+﻿const CACHE='mt-mq-v20260831131000';
 const CORE=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 const AUDIO=[
   "./qaris/manshawy/audio/01.mp3",
