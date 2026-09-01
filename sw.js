2cc3fea10fee9ce96dd751c47a0c925955d12104
mansourfatemi <mansour.afghan@gmail.com>
2026-09-01 08:32:27 +0200

    fix: ctrl stays at bottom of card using flex layout with min-height:0

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='mt-mq-v20260901083003';
+const CACHE='mt-mq-v20260901083221';
 const CORE=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 const AUDIO=[
   "./qaris/manshawy/audio/01.mp3",
