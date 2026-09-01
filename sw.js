e41628c32a18e6ea657e7938f3d9074756bbd821
mansourfatemi <mansour.afghan@gmail.com>
2026-09-01 12:07:40 +0200

    Fix ZWNJ in Mustafa Ismail id=3 translations (43 fixes) + fix fraz 81 closing phrase

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='mt-mq-v20260901092258';
+const CACHE='mt-mq-v20260901120732';
 const CORE=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 const AUDIO=[
   "./qaris/manshawy/audio/01.mp3",
