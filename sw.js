717051382d9ea67191595402d16106868e6ab694
mansourfatemi <mansour.afghan@gmail.com>
2026-09-04 16:52:34 +0200

    feat(maqamat): populate Shahhat Anwar and Abdulbasit Abdulsamad in qari-picker with extracted maqam-master audio clips from legacy app database (bayati, rast, hijaz, saba, segah, nahawand)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='mt-mq-v20260904163026';
+const CACHE='mt-mq-v20260904165147';
 const CORE=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 const AUDIO=[
   "./qaris/manshawy/audio/01.mp3",
