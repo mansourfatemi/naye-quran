28f1dc00f5c8210795df69b59941cf21607e16ea
mansourfatemi <mansour.afghan@gmail.com>
2026-09-07 13:15:09 +0200

    Fix 3 ayah errors (Mustafa Ismail): ازحمهما->ارحمهما (Isra 70,75), add لهوَ (Luqman 16); Tuner v2 AudioWorklet+overlap

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='mt-mq-v20260904213447';
+const CACHE='mt-mq-v20260907131452';
 const CORE=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 const AUDIO=[
   "./qaris/manshawy/audio/01.mp3",
