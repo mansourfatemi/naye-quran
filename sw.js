0164e32e004eb8508160e7fd9e9a8d0d08939c35
mansourfatemi <mansour.afghan@gmail.com>
2026-09-09 14:20:23 +0200

    Fix critical JS syntax error in formatTajweedMatn regex + halya sections working

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='mt-mq-v20260909141551';
+const CACHE='mt-mq-v20260909142001';
 const CORE=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 const AUDIO=[
   "./qaris/manshawy/audio/01.mp3",
